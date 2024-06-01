ecbf2c804c8ab3d6db736138abd7ea60045162d0
KhairilRahman04 <KhairilRahman04>
2024-06-01 11:31:07 +0700

    feat: option winpoint and add comment

--- 01.DOM (Practice Making Scoreboard Bag)/app.js
+++ 01.DOM (Practice Making Scoreboard Bag)/app.js
@@ -3,13 +3,15 @@ const p2Button = document.querySelector('#p2-button');
 const p1Display = document.querySelector('#p1-display');
 const p2Display = document.querySelector('#p2-display');
 const resetButton = document.querySelector('#reset')
+const winPointOption = document.querySelector('#winpoint');
+
 
 let p1Score = 0;
 let p2Score = 0;
-let winPoint = 5;
+let winPoint = 3;
 let isGameOver = false;
 
-
+//fungsi reset
 function reset () {
     isGameOver = false;
     p1Score = 0;
@@ -18,6 +20,8 @@ function reset () {
     p2Display.textContent = 0;
 }
 
+//counter p1 dengan logic: ketika game belum berakhir maka p1score bisa di klik(bertambah).
+//jika p1score sama dengan winpoint maka game berakhir(tidak bisa bertambah) 
 p1Button.addEventListener('click',function() {
     if (!isGameOver){
         p1Score += 1;
@@ -27,6 +31,8 @@ p1Button.addEventListener('click',function() {
     p1Display.textContent = p1Score;
 });
 
+//counter p2 dengan logic: ketika game belum berakhir maka p2score bisa di klik(bertambah).
+//jika p2score sama dengan winpoint maka game berakhir(tidak bisa bertambah) 
 p2Button.addEventListener('click', function() {
     if (!isGameOver){
         p2Score += 1;
@@ -36,4 +42,13 @@ p2Button.addEventListener('click', function() {
     p2Display.textContent = p2Score;
 })
 
+//reset display
 resetButton.addEventListener('click',reset)
+
+
+//ketika option di ganti maka akan mengubah nilai winpoint berdasarkan value dari option
+//lalu reset display ketika option di ganti
+winPointOption.addEventListener('change', function(){
+    winPoint = parseInt(this.value)
+    reset()
+});
