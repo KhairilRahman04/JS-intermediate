cd9b135ff4fb13fe40a1168aba7287ab63c20b01
KhairilRahman04 <KhairilRahman04>
2024-06-01 10:58:16 +0700

    logic limit win point

--- 01.DOM (Practice Making Scoreboard Bag)/app.js
+++ 01.DOM (Practice Making Scoreboard Bag)/app.js
@@ -5,13 +5,23 @@ const p2Display = document.querySelector('#p2-display');
 
 let p1Score = 0;
 let p2Score = 0;
+let winPoint = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click',function() {
-    p1Score += 1;
+    if (!isGameOver){
+        p1Score += 1;
+    }if (p1Score === winPoint){
+        isGameOver = true;
+    }
     p1Display.textContent = p1Score;
 })
 
 p2Button.addEventListener('click', function() {
-    p2Score += 1;
+    if (!isGameOver){
+        p2Score += 1;
+    }if (p2Score === winPoint){
+        isGameOver = true;
+    }
     p2Display.textContent = p2Score;
 })
