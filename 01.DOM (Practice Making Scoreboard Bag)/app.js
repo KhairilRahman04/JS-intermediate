c3db540af511ec510b1e101dd11d8fbe8f60fe70
KhairilRahman04 <KhairilRahman04>
2024-06-01 10:30:58 +0700

    setup DOM practice

---
+++ 01.DOM (Practice Making Scoreboard Bag)/app.js
@@ -0,0 +1,4 @@
+const p1Button = document.querySelector('#p1-button');
+const p2Button = document.querySelector('#p2-button');
+const p1Display = document.querySelector('#p1-display');
+const p2Display = document.querySelector('#p2-display');
