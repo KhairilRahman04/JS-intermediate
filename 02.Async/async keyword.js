43358710420327fff7cfa2c9c63bf9cb05ca6056
KhairilRahman04 <KhairilRahman04>
2024-06-03 16:36:00 +0700

    async

---
+++ 02.Async/async keyword.js
@@ -0,0 +1,14 @@
+
+const hello = async () => {
+    return ("hello world");//jika berhasil
+    //throw 'gagal' -> //jika gagal
+}
+
+hello()
+.then((res) => {//then jika berhasil
+    console.log('respone:',res)
+})
+
+// .catch(()=>{
+    //catch hanya jika gagal
+// })
