352bf415cab35ee5ecefe6510ac944e3bb76468a
KhairilRahman04 <KhairilRahman04>
2024-06-06 22:01:35 +0700

    request menggunakan fetch

---
+++ 03.ajaj/Request Dengan Fetch/app.js
@@ -0,0 +1,16 @@
+//membuat request menggunakan fetch
+
+const req = async () => {
+    try{
+        const data1 = await fetch('https://swapi.dev/api/people/1/')
+        const datajson1 = await data1.json()
+        console.log(datajson1)
+        const data2 = await fetch('https://swapi.dev/api/people/2/')
+        const datajson2 = await data2.json()
+        console.log(datajson2)
+    }
+    catch(err){
+        console.log('error',err)
+    }
+}
+req()
